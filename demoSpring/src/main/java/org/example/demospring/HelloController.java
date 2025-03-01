571294e8c2ee6d0babcb2eabe03aae8fb6d3ac71
bharat1202 <152322224+bharat1202@users.noreply.github.com>
2025-03-01 16:35:37 +0530

    [bharat]commiting

--- demoSpring/src/main/java/org/example/demospring/HelloController.java
+++ demoSpring/src/main/java/org/example/demospring/HelloController.java
@@ -1,13 +1,21 @@
 package org.example.demospring;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-@RequestMapping
+@RequestMapping("/hello")
 public class HelloController {
-    @GetMapping("/hello")
+
+    // curl localhost:8080/hello -w "\n"
+    @RequestMapping(value = {"", "/", "/home"})
     public String sayHello() {
-        return "Hello Bridgelabz!";
+        return "Hello From BridgeLabz!!!";
     }
 
+    // curl localhost:8080/hello/query?name=Narayan -w "\n"
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
+    }
 }
