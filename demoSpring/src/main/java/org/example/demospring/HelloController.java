ed9ce8eb512b5cf467df68ed30e3b7e7edf19212
bharat1202 <152322224+bharat1202@users.noreply.github.com>
2025-03-01 23:42:05 +0530

    [bharat]commiting

--- demoSpring/src/main/java/org/example/demospring/HelloController.java
+++ demoSpring/src/main/java/org/example/demospring/HelloController.java
@@ -29,5 +29,9 @@ public class HelloController {
     public String sayHello(@RequestBody User user) {
         return "Hello " + user.getFirstName() + " " + user.getLastName() + "!";
     }
-
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable String firstName,
+                           @RequestParam(value = "lastName") String lastName) {
+        return "Hello " + firstName + " " + lastName + "!";
+    }
 }
